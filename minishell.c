109f477f63e85795f97b29fcdb312eba99055562
Zulfi12345 <a1850245@adelaide.edu.au>
2024-08-07 16:24:21 +0930

    v2

--- minishell.c
+++ minishell.c
@@ -1,11 +1,10 @@
 /*********************************************************************
-   Program  : miniShell                   Version    : 1.3
- --------------------------------------------------------------------
-   skeleton code for linix/unix/minix command line interpreter
- --------------------------------------------------------------------
-   File			: minishell.c
-   Compiler/System	: gcc/linux
-
+Program : miniShell Version : 1.3
+--------------------------------------------------------------------
+skeleton code for linux/unix/minix command line interpreter
+--------------------------------------------------------------------
+File : minishell.c
+Compiler/System : gcc/linux
 ********************************************************************/
 
 #include <sys/types.h>
@@ -15,129 +14,121 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <errno.h>
 
-#define NV 20            /* max number of command tokens */
-#define NL 100           /* input buffer size */
-char line[NL];           /* command input buffer */
-int job_counter = 1;     /* job counter for background processes */
-int jobs[NL];            /* store PIDs of background processes */
-int job_ids[NL];         /* store job ids for background processes */
-int job_count = 0;       /* count of active background jobs */
+#define NV 20  /* max number of command tokens */
+#define NL 100 /* input buffer size */
+#define MAX_JOBS 100
 
-/*
-    shell prompt
- */
+char line[NL]; /* command input buffer */
+
+/* Job structure to hold job ID and PID */
+typedef struct {
+    int job_id;
+    pid_t pid;
+} Job;
+
+Job jobs[MAX_JOBS];
+int job_count = 0;
+int next_job_id = 1;
+
+/* shell prompt */
 void prompt(void) {
-    fprintf(stdout, "\n msh> ");
     fflush(stdout);
 }
 
-void handle_background_completion(int sig) {
-    int status;
+/* Handle SIGCHLD to report background process completion */
+void handle_sigchld(int sig) {
+    int saved_errno = errno;
     pid_t pid;
-
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+    while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
         for (int i = 0; i < job_count; i++) {
-            if (jobs[i] == pid) {
-                printf("\n[%d]+ Done %s\n", job_ids[i], line);
-                fflush(stdout);
-                jobs[i] = jobs[job_count - 1];
-                job_ids[i] = job_ids[job_count - 1];
+            if (jobs[i].pid == pid) {
+                printf("\n[%d] Done %d\n", jobs[i].job_id, pid);
+                /* Remove the job from the list */
+                for (int j = i; j < job_count - 1; j++) {
+                    jobs[j] = jobs[j + 1];
+                }
                 job_count--;
+                if (job_count == 0) {
+                    next_job_id = 1; // Reset job ID when no background jobs are running
+                }
                 break;
             }
         }
-        prompt();
     }
+    errno = saved_errno;
 }
 
-int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;    /* value returned by fork sys call */
-    int wpid;         /* value returned by wait */
-    char *v[NV];      /* array of pointers to command line tokens */
-    char *sep = " \t\n";  /* command line token separators    */
-    int i;            /* parse index */
-    int is_background; /* background process flag */
+int main(int argc, char *argv[], char *envp[]) {
+    int frkRtnVal;       /* value returned by fork sys call */
+    int wpid;            /* value returned by wait */
+    char *v[NV];         /* array of pointers to command line tokens */
+    char *sep = " \t\n"; /* command line token separators */
+    int i;               /* parse index */
 
-    signal(SIGCHLD, handle_background_completion);
+    // // Handle SIGCHLD to report background process completion
+    // struct sigaction sa;
+    // sa.sa_handler = &handle_sigchld;
+    // sigemptyset(&sa.sa_mask);
+    // sa.sa_flags = SA_RESTART;
+    // if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    //     perror("sigaction");
+    //     exit(1);
+    // }
 
-    /* prompt for and process one command line at a time  */
-    while (1) {            /* do Forever */
+    /* prompt for and process one command line at a time */
+    while (1) {
         prompt();
-        if (fgets(line, NL, stdin) == NULL) {
-            if (feof(stdin)) {        /* non-zero on EOF  */
-                fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(), feof(stdin), ferror(stdin));
-                exit(0);
-            }
-            perror("fgets");
-            continue;
+        fgets(line, NL, stdin);
+        fflush(stdin);
+        if (feof(stdin)) {
+            exit(0);
         }
-
         if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-            continue;            /* to prompt */
-
+            continue; /* to prompt */
         v[0] = strtok(line, sep);
         for (i = 1; i < NV; i++) {
             v[i] = strtok(NULL, sep);
             if (v[i] == NULL)
                 break;
         }
-
-        is_background = 0;
-        if (i > 0 && strcmp(v[i - 1], "&") == 0) {
-            is_background = 1;
-            v[i - 1] = NULL;
+        /* assert i is number of tokens + 1 */
+        int background = 0;
+        if (i > 1 && strcmp(v[i - 1], "&") == 0) {
+            background = 1;
+            v[i - 1] = NULL; // Remove the '&' token
         }
 
         if (strcmp(v[0], "cd") == 0) {
             if (v[1] == NULL) {
-                fprintf(stderr, "cd: missing operand\n");
-            } else {
-                if (chdir(v[1]) != 0) {
-                    perror("chdir");
-                }
+                fprintf(stderr, "cd: missing argument\n");
+            } else if (chdir(v[1]) != 0) {
+                perror("cd");
             }
             continue;
         }
 
+        /* fork a child process to exec the command in v[0] */
         switch (frkRtnVal = fork()) {
-            case -1:            /* fork returns error to parent process */
-                perror("fork");
-                break;
-            case 0:            /* code executed only by child process */
-                if (execvp(v[0], v) == -1) {
-                    perror("execvp");
-                    exit(EXIT_FAILURE); /* Terminate child process if exec fails */
-                }
-                break;
-            default:            /* code executed only by parent process */
-                if (is_background) {
-                    jobs[job_count] = frkRtnVal;
-                    job_ids[job_count] = job_counter++;
-                    printf("[%d] %d\n", job_ids[job_count], frkRtnVal);
-                    fflush(stdout);
-                    job_count++;
-                } else {
-                    wpid = waitpid(frkRtnVal, NULL, 0);
-                    if (wpid == -1) {
-                        perror("waitpid");
-                    }
-                    printf("%s done\n", v[0]);
-                }
-
-                /* Check for completed background processes */
-                while ((wpid = waitpid(-1, NULL, WNOHANG)) > 0) {
-                    for (int i = 0; i < job_count; i++) {
-                        if (jobs[i] == wpid) {
-                            printf("[%d]+ Done %s\n", job_ids[i], line);
-                            jobs[i] = jobs[job_count - 1];
-                            job_ids[i] = job_ids[job_count - 1];
-                            job_count--;
-                            break;
-                        }
-                    }
-                }
-                break;
-        }                /* switch */
-    }                /* while */
-}                /* main */
+        case -1: /* fork returns error to parent process */
+            perror("fork");
+            break;
+        case 0: /* code executed only by child process */
+            execvp(v[0], v);
+            perror("execvp");
+            exit(1);
+        default: /* code executed only by parent process */
+            if (background) {
+                jobs[job_count].job_id = next_job_id++;
+                jobs[job_count].pid = frkRtnVal;
+                job_count++;
+                printf("[%d] %d\n", jobs[job_count - 1].job_id, frkRtnVal);
+            } else {
+                wpid = waitpid(frkRtnVal, NULL, 0);
+            }
+            break;
+        } /* switch */
+    } /* while */
+    return 0;
+} /* main */
